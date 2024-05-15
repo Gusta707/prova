7edbefd108bf94c47602d6dafe80854c9ba127e9
Gustavo Monteiro <135048070+Gusta707@users.noreply.github.com>
2024-05-15 18:44:18 -0300

    Update prova.sql

--- gincana-prova/prova.sql
+++ gincana-prova/prova.sql
@@ -9,3 +9,26 @@ update Livros
 set disponivel = false
 where ano_publicao <1980;
 
+# 3 
+update Livros 
+set editora = 'Plume Books'
+where titulo = '1980';
+
+# 4 
+delete from Livros 
+where idioma = 'Inglês', ano_publicacao < 2000;
+
+# 5 
+select * from Livros
+where quantidade_paginas > 650;
+
+# 6
+select * from Livros
+where categoria = 'Drama'
+group by titulos;
+
+
+# 9
+select * from Livros
+where ano_publicacao
+order by titulos desc;
